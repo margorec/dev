b66036811d1f9aae7983e5c98db7de7fa1a74ff0
Marcin Gorecki <marcin_gorecki@epam.com>
2017-11-15 15:37:15 +0100

    Added README.md

--- ChartAnalysis/src/main/java/com/marcingorecki/ChartAnalysis/controller/WelcomeController.java
+++ ChartAnalysis/src/main/java/com/marcingorecki/ChartAnalysis/controller/WelcomeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
 import java.util.Map;
 
 
@@ -20,8 +21,8 @@ public class WelcomeController {
 
     @RequestMapping("/")
     public String welcome(Map<String, Object> model) {
-        parser.parseToTimeseries();
-        model.put("message", "check console");
+        Map<String, Double> data = parser.parseToTimeseries();
+        model.put("chartData", data);
         return "welcome";
     }
 
--- ChartAnalysis/src/main/java/com/marcingorecki/ChartAnalysis/service/Parser.java
+++ ChartAnalysis/src/main/java/com/marcingorecki/ChartAnalysis/service/Parser.java
@@ -3,13 +3,18 @@ package com.marcingorecki.ChartAnalysis.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Service
 public class Parser {
 
-    private final String NEW_LINE = "\\r?\\n";
-
+    private static final String FIELD_DELIMITER = "," ;
+    private static final String NEW_LINE_DELIMITER = "\\r?\\n";
+    private static final DateTimeFormatter FORMATTER  = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     private final Downloader downloader;
 
     @Autowired
@@ -17,11 +22,25 @@ public class Parser {
         this.downloader = downloader;
     }
 
-    public void parseToTimeseries() {
+    public Map<String, Double> parseToTimeseries() {
         String data = downloader.download();
-        String[] lines = data.split(NEW_LINE);
-        Arrays.stream(lines).forEach(System.out::println);
+        return fetchDateAndFinalPrice(data);
+    }
 
+    private Map<String, Double> fetchDateAndFinalPrice(String data) {
+        Map<String, Double> result = new LinkedHashMap<>();
+        String[] lines = data.split(NEW_LINE_DELIMITER);
+        Arrays.stream(lines).skip(1).forEach(line -> {
 
+            String[] fields = line.split(FIELD_DELIMITER);
+            System.out.println(fields[0]);
+            result.put(parseDate(fields[0]), Double.valueOf(fields[3]));
+        });
+        return result;
+    }
+
+    private String parseDate(String dateString) {
+        LocalDate date = LocalDate.parse(dateString);
+        return date.format(FORMATTER);
     }
 }
